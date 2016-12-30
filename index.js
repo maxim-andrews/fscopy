d5ff1e71c0bc6c58049299d0383b466900d49483
Maxim Andrews <all.git@am.id.lv>
2016-12-30 22:16:00 +0000

    Options added & improved cyclomatic complexity

--- index.js
+++ index.js
@@ -1,32 +1,62 @@
-module.exports = exports = function copyFileSystem(pathname, srcFS, dstFS) {
+module.exports = exports = function copyFileSystem(pathname, srcFS, dstFS, options) {
+  options = options || {};
+  
   let srcStat, dstStat, content;
 
-  console.log(pathname);
+  if (options.debug) {
+    console.log(pathname);
+  }
 
   try {
     srcStat = srcFS.statSync(pathname);
-  } catch (e) {}
+  } catch (e) {
+    if (options.debug > 1) {
+      throw e;
+    }
+  }
 
   try {
     dstStat = dstFS.statSync(pathname);
-  } catch (e) {}
-
-  if (srcStat && srcStat.isFile() && !dstStat) {
-    if (!srcStat.isDirectory()) {
-      dstFS.writeFileSync(pathname, srcFS.readFileSync(pathname, 'utf8'), {
-        mode: srcStat.mode,
-        encoding: 'utf8'
-      });
-    } else {
-      dstFS.mkdirSync(pathname, srcStat.mode);
-
-      content = srcFS.readdirSync(pathname);
-
-      if (Array.isArray(content)) {
-        content.forEach(elm => {
-          copyFileSystem(path.join(pathname, elm), srcFS, dstFS);
-        });
-      }
+  } catch (e) {
+    if (options.debug > 1) {
+      throw e;
     }
+  }
+
+  if (!srcStat || !srcStat.isFile() && !srcStat.isDirectory() || dstStat && !options.overwrite) {
+    return false;
+  }
+
+  if (dstStat) {
+    remove(pathname, dstStat, dstFS);
+  }
+
+  if (!srcStat.isDirectory()) {
+    return dstFS.writeFileSync(pathname, srcFS.readFileSync(pathname, 'utf8'), {
+      mode: srcStat.mode,
+      encoding: 'utf8'
+    });
+  }
+
+  dstFS.mkdirSync(pathname, srcStat.mode);
+
+  copydircontent(pathname, srcFS, dstFS);
+}
+
+function copydircontent(pathname, srcFS, dstFS) {
+  content = srcFS.readdirSync(pathname);
+
+  if (Array.isArray(content)) {
+    content.forEach(elm => {
+      copyFileSystem(path.join(pathname, elm), srcFS, dstFS);
+    });
+  }
+}
+
+function remove(pathname, stat, fs) {
+  if (stat.isDirectory()) {
+    fs.rmdirSync(pathname);
+  } else if (stat.isFile()) {
+    fs.unlinkSync(pathname);
   }
 }
